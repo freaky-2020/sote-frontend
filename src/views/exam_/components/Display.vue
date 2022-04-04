f190744001f93186c63b01a876282e3869d76bd0
2639043230 <2639043230@qq.com>
2022-04-04 16:34:11 +0800

    提交测试

--- src/views/exam_/components/Display.vue
+++ src/views/exam_/components/Display.vue
@@ -1,18 +1,17 @@
 <template>
   <el-card>
     <div>
-      <div class="common-btn">
         <!--      @click是v-on:click的缩写，这时v-for循环的index就起到了用处，将所点击的button的index下标传到clickbtnX方法内-->
         <!--      :type是v-bind:type的缩写，其意思是判断flagX是否等于index+1，如果等于，加上名为primary的样式，如果不等于则空-->
         <!--      v-for需要有:key="item.id，否则会报错"-->
-        <div slot="header" class="clearfix">
-          <h3 class="box-center">单选题（共1题，0.00分）</h3>
-        </div>
-        1.给书柜定位时，宜用mm定位线
-        <div v-for="(item,index) in navList" :key="item.id" @click="clickbtnX(index)">
-          <el-button class="elbtn" size="mini" circle :type="flagX===(index+1)?'primary':''" >{{ item.name }}</el-button>
-          sdfsd
-        </div>
+      <a @click="goAnchor('#masd')">点击这里跳转</a>
+      <div slot="header" class="clearfix">
+        <h3 class="box-center">单选题（共1题，0.00分）</h3>
+      </div>
+      1.给书柜定位时，宜用mm定位线
+      <div v-for="(item,index) in navList" :key="item.id" @click="clickbtnX(index)">
+        <el-button class="elbtn" size="mini" circle :type="flagX===(index+1)?'primary':''" >{{ item.name }}</el-button>
+        sdfsd
       </div>
       <div slot="header" class="clearfix">
         <h3 class="box-center">多选题（共1题，0.00分）</h3>
@@ -25,6 +24,16 @@
       1.给书柜定位时，宜用mm定位线
       <div v-for="(item,index) in navList" :key="item.id" @click="clickbtnX(index)">
         <el-button class="elbtn" size="mini" circle :type="flagX===(index+1)?'primary':''" >{{ item.name }}</el-button>
+        sdfsd
+      </div>
+      1.给书柜定位时，宜用mm定位线
+      <div v-for="(item,index) in navList" :key="item.id" @click="clickbtnX(index)">
+        <el-button class="elbtn" size="mini" circle :type="flagX===(index+1)?'primary':''" >{{ item.name }}</el-button>
+        sdfsd
+      </div>
+      1.给书柜定位时，宜用mm定位线
+      <div v-for="(item,index) in navList" :key="item.id" @click="clickbtnX(index)">
+        <el-button id="masd" class="elbtn" size="mini" circle :type="flagX===(index+1)?'primary':''" >{{ item.name }}</el-button>
         sdfsd
       </div>
     </div>
@@ -46,7 +55,13 @@ export default {
   methods: {
     clickbtnX(index) {
       this.flagX = index + 1
-    }
+    },
+    //模拟锚点跳转
+    goAnchor(selector) {//参数selector是id选择器（#anchor14）
+      document.querySelector(selector).scrollIntoView({
+        behavior: "smooth"
+      });
+    },
   }
 }
 </script>
